94d6e6e9c989d32b0b2f265aa8b170961c4d0f47
LeandroDukievicz <ldukie@icloud.com>
2024-06-19 22:50:31 -0300

    adicionando pasta routes e definindo rotas

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+
+
 const db = require('./database')
+const routes = require('./routes')
 
 const app = express()
 
 // conectando banco de dados 
 db.connect()
-
 
 //Definindo o template engine
 app.set('view engine', 'ejs')
@@ -19,12 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //habilitando server pra receber dados via post (form)
 app.use(express.urlencoded({extended: true}))
 
-//Rotas
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Titulo Teste'
-    })
-})
+// Definindo Rotas 
+app.use('/', routes)
 
 // Error404 (Not Found)
 app.use((req, res) => {
@@ -32,7 +31,5 @@ app.use((req, res) => {
 })
 
 //Executando servidor
-
-
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
