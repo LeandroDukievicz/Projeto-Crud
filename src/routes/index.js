2ece37e942e3e4118b2953424f43f973943c2c50
LeandroDukievicz <ldukie@icloud.com>
2024-06-25 15:35:17 -0300

    adicionando funcao para remocao de usuarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,9 +13,10 @@ router.post('/register/add', CustomersController.add)
 
 // listar 
 router.get('/list', CustomersController.list)
-
 //editar 
 router.get('/edit', CustomersController.formEdit)
 router.post('/edit/:id', CustomersController.edit)
+//remover
+router.get('/remove/:id', CustomersController.remove)
 
 module.exports = router
